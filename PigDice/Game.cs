99d077534e957eddb4c34f40f2f25ca60674fd08
Igor <nrovo7@gmail.com>
2021-01-26 14:29:00 +0200

    All warnings are fixed. Code is refactored. A part of functionality of PlayGame method from Game class was moved to new SwitchPlayer mathod.

--- PigDice/Game.cs
+++ PigDice/Game.cs
@@ -7,81 +7,96 @@ namespace PigDice
 {
     public class Game
     {
-        private List<Player> Players { get; set; }
-        private Random random = new Random();
-        private static readonly string[][] diceDrawings;
+        private List<Player> Players { get; }
+        private readonly Random _random = new Random();
+        private static readonly string[][] _diceDrawings;
+        private bool GameComplete { get; set; }
+        private Player WinningPlayer { get; set; }
 
-        public Game(List <Player> players)
+        private Game(List<Player> players)
         {
             Players = players;
         }
 
         static Game()
         {
-            diceDrawings = new string[6][];
+            _diceDrawings = new string[6][];
 
-            diceDrawings[0] = new[] {
-                    "┏───────┑",
-                    "┃       ┃",
-                    "┃   O   ┃",
-                    "┃       ┃",
-                    "┖───────┙"};
+            _diceDrawings[0] = new[]
+            {
+                "┏───────┑",
+                "┃       ┃",
+                "┃   O   ┃",
+                "┃       ┃",
+                "┖───────┙"
+            };
 
-            diceDrawings[1] = new[] {
-                    "┏───────┑",
-                    "┃ O     ┃",
-                    "┃       ┃",
-                    "┃     O ┃",
-                    "┖───────┙"};
+            _diceDrawings[1] = new[]
+            {
+                "┏───────┑",
+                "┃ O     ┃",
+                "┃       ┃",
+                "┃     O ┃",
+                "┖───────┙"
+            };
 
-            diceDrawings[2] = new[] {
-                    "┏───────┑",
-                    "┃ O     ┃",
-                    "┃   O   ┃",
-                    "┃     O ┃",
-                    "┖───────┙"};
+            _diceDrawings[2] = new[]
+            {
+                "┏───────┑",
+                "┃ O     ┃",
+                "┃   O   ┃",
+                "┃     O ┃",
+                "┖───────┙"
+            };
 
-            diceDrawings[3] = new[] {
-                    "┏───────┑",
-                    "┃ O   O ┃",
-                    "┃       ┃",
-                    "┃ O   O ┃",
-                    "┖───────┙"};
+            _diceDrawings[3] = new[]
+            {
+                "┏───────┑",
+                "┃ O   O ┃",
+                "┃       ┃",
+                "┃ O   O ┃",
+                "┖───────┙"
+            };
 
-            diceDrawings[4] = new[] {
-                    "┏───────┑",
-                    "┃ O   O ┃",
-                    "┃   O   ┃",
-                    "┃ O   O ┃",
-                    "┖───────┙"};
+            _diceDrawings[4] = new[]
+            {
+                "┏───────┑",
+                "┃ O   O ┃",
+                "┃   O   ┃",
+                "┃ O   O ┃",
+                "┖───────┙"
+            };
 
-            diceDrawings[5] = new[] {
-                    "┏───────┑",
-                    "┃ O   O ┃",
-                    "┃ O   O ┃",
-                    "┃ O   O ┃",
-                    "┖───────┙"};
+            _diceDrawings[5] = new[]
+            {
+                "┏───────┑",
+                "┃ O   O ┃",
+                "┃ O   O ┃",
+                "┃ O   O ┃",
+                "┖───────┙"
+            };
         }
 
-        public void PrintTurnStart(int currentPlayer)
+        private void PrintTurnStart(int currentPlayer)
         {
-            for(var index = 0; index < Players.Count; index++)
+            for (var index = 0; index < Players.Count; index++)
             {
-                Player player = Players[index];
+                var player = Players[index];
                 Console.ForegroundColor = White;
                 Console.Write(currentPlayer == index ? "-->" : "   ");
                 Console.ForegroundColor = player.Color;
-                Console.WriteLine("{0,-15}{1}", player.Name, player.TotalScore);
+                Console.WriteLine("{0,-15}{1}", player.Name, player.TotalScore.ToString());
             }
+
             Console.ForegroundColor = White;
         }
 
-        private void PrintDice(int value1, int value2)
+        private static void PrintDice(int value1, int value2)
         {
             Console.ForegroundColor = White;
             for (var row = 0; row < 5; row++)
             {
-                Console.WriteLine(diceDrawings[value1 - 1][row] + "    " + diceDrawings[value2 - 1][row]);
+                Console.WriteLine($"{_diceDrawings[value1 - 1][row].ToString()}    {_diceDrawings[value2 - 1][row].ToString()}");
             }
         }
 
@@ -100,12 +115,12 @@ namespace PigDice
         private static void PrintRoundInformation(Player player)
         {
             Console.ForegroundColor = player.Color;
-            Console.WriteLine(player.Name + " has " + player.RoundScore + " this round.");
+            Console.WriteLine($"{player.Name} has {player.RoundScore.ToString()} this round.");
             Console.ForegroundColor = White;
             Console.WriteLine("Press <Space> to keep going or <Enter> to hold.");
         }
 
-        private bool HasWon(Player player)
+        private static bool HasWon(Player player)
         {
             return player.TotalScore >= 100;
         }
@@ -113,62 +128,70 @@ namespace PigDice
         public void PlayGame()
         {
             var playerIndex = 0;
-            var gameComplete = false;
-            Player winningPlayer = null;
+            GameComplete = false;
+            WinningPlayer = null;
 
-            while(!gameComplete)
+            while (!GameComplete)
             {
                 Console.Clear();
                 PrintTurnStart(playerIndex);
-                Player currentPlayer = Players[playerIndex];
+                var currentPlayer = Players[playerIndex];
                 Console.WriteLine("Press any key to start your turn.");
                 Console.ReadKey(false);
-
-                while(true)
-                {
-                    Console.Clear();
-                    PrintTurnStart(playerIndex);
-                    var die1 = random.Next(6) + 1;
-                    var die2 = random.Next(6) + 1;
-                    PrintDice(die1, die2);
-                    
-                    if(die1 == 1 || die2 == 1)
-                    {
-                        currentPlayer.RoundScore = 0;
-                        playerIndex = (playerIndex + 1) % Players.Count;
-                        PrintPigMessage();
-                        break;
-                    }
-
-                    currentPlayer.RoundScore += die1 + die2;
-                    PrintRoundInformation(currentPlayer);
-                    ConsoleKeyInfo keyInfo = Console.ReadKey(false);
-
-                    if(keyInfo.Key == ConsoleKey.Enter)
-                    {
-                        currentPlayer.TotalScore += currentPlayer.RoundScore;
-                        Players[playerIndex].RoundScore = 0;
-
-                        if(HasWon(currentPlayer))
-                        {
-                            winningPlayer = currentPlayer;
-                            gameComplete = true;
-                        }
-
-                        playerIndex = (playerIndex + 1) % Players.Count;
-                        break;
-                    }
-                }
+                playerIndex = SwitchPlayer(playerIndex, currentPlayer);
             }
-            Console.WriteLine(winningPlayer.Name + " has won!");
+            Console.WriteLine($"{WinningPlayer.Name} has won!");
             Console.ReadKey();
+        }
+
+        private int SwitchPlayer(int playerIndex, Player currentPlayer)
+        {
+            while (true)
+            {
+                Console.Clear();
+                PrintTurnStart(playerIndex);
+                var die1 = _random.Next(6) + 1;
+                var die2 = _random.Next(6) + 1;
+                PrintDice(die1, die2);
+
+                if (die1 == 1 || die2 == 1)
+                {
+                    currentPlayer.RoundScore = 0;
+                    playerIndex = (playerIndex + 1) % Players.Count;
+                    PrintPigMessage();
+                    break;
+                }
+
+                currentPlayer.RoundScore += die1 + die2;
+                PrintRoundInformation(currentPlayer);
+                var keyInfo = Console.ReadKey(false);
+
+                if (keyInfo.Key != ConsoleKey.Enter)
+                {
+                    continue;
+                }
+
+                currentPlayer.TotalScore += currentPlayer.RoundScore;
+                Players[playerIndex].RoundScore = 0;
+
+                if (HasWon(currentPlayer))
+                {
+                    WinningPlayer = currentPlayer;
+                    GameComplete = true;
+                }
+
+                playerIndex = (playerIndex + 1) % Players.Count;
+                break;
+            }
+
+            return playerIndex;
         }
 
         public static Game CreateGame(int numberOfPlayers)
         {
             var allPlayers = new[] {Red, Blue, Yellow, Green}
                .Take(Math.Max(2, Math.Min(4, numberOfPlayers)))
-               .Select((color, index) => new Player($"Player {index + 1}", color))
+               .Select((color, index) => new Player($"Player {(index + 1).ToString()}", color))
                .ToList();
 
             return new Game(allPlayers);
--- PigDice/Program.cs
+++ PigDice/Program.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PigDice
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private static void Main()
         {
             Console.Write("How many players are there? ");
             var amount = Convert.ToInt32(Console.ReadLine());
 
-            Game game = Game.CreateGame(amount);
+            var game = Game.CreateGame(amount);
 
             game.PlayGame();
         }
